8fd6690469b3bf2ee793ac31a36a63719b60c298
Cesar Augusto Dell Antonio Jr <CesarJr@MBP-de-Cesar.hitronhub.home>
2018-08-12 19:14:59 -0700

    setup formating for amount and date

--- src/components/CompaniesListItem.js
+++ src/components/CompaniesListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const CompaniesListItem = (props) => (
     <div>
         <Link to={`/lista/${props.id}`}>{props.name}</Link>
         <p>Atividade Principal: {props.activity}</p>
-        <p>Faturamento Líquido: {props.revenue}</p>
-        <p>Último Demonstrativo Financeiro: {props.lastFinancialReport}</p>
+        <p>Faturamento Líquido: {numeral(props.revenue / 100).format('$0,0.00')}</p>
+        <p>Último Demonstrativo Financeiro: {moment(props.lastFinancialReport).format('MM/YYYY')}</p>
     </div>
 );
 
